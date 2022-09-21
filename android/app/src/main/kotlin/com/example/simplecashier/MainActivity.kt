5cd33a0419aff74da74c614c52c144c2e2e194bb
Khalid Ali <khalidchaudry130@gmail.com>
2022-09-21 16:02:48 +0500

    Build project

---
+++ android/app/src/main/kotlin/com/example/simplecashier/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.simplecashier
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:simplecashier/view/screens/home_screen/home_screen.dart';
+main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Simple Cashier',
+      
+      home:  HomeScreen(),
+    );
+  }
+}
